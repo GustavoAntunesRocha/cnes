2c91501ea7395f30ffa348cc84027beb420fdb67
Gustavo <gustavo.antunes.rocha@outlook.com>
2021-08-12 15:50:54 -0300

    Criação do projeto de carregamento das planilhas

---
+++ Projetos/cnesCarregamentoDados/src/main/java/br/com/gustavo/cnesCarregamentoDados/CnesCarregamentoDadosApplication.java
@@ -0,0 +1,13 @@
+package br.com.gustavo.cnesCarregamentoDados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnesCarregamentoDadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CnesCarregamentoDadosApplication.class, args);
+	}
+
+}
---
+++ Projetos/cnesCarregamentoDados/src/test/java/br/com/gustavo/cnesCarregamentoDados/CnesCarregamentoDadosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gustavo.cnesCarregamentoDados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnesCarregamentoDadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
